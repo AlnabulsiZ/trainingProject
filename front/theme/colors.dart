89adf4a6869dcf39d93e598634c3b1a3caf802f2
AlnabulsiZ <zina.nabulsi04@gmail.com>
2025-03-21 01:12:15 +0300

    Add files via upload

---
+++ front/theme/colors.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/widgets.dart';
+class AppColors {
+  static const Color backC = Color(0xFFEFF0F2);
+  static const Color blueC = Color(0xFF2BA5BC);
+  static const Color brownC = Color(0xFF5C564B);
+}
