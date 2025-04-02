94f68bcfd55b984e2ea58710b852da9ebd393134
AlnabulsiZ <zina.nabulsi04@gmail.com>
2025-04-02 16:06:09 +0300

    Add files via upload

--- front/services/auth_service.dart
+++ front/services/auth_service.dart
@@ -1,1 +1,37 @@
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+import 'package:shared_preferences/shared_preferences.dart';
 
+class AuthService {
+  static const String _baseUrl = 'http://127.0.0.1:8000';
+
+
+  static Future<void> saveToken(String token) async {
+    final prefs = await SharedPreferences.getInstance();
+    await prefs.setString('auth_token', token);
+  }
+
+  static Future<String?> getToken() async {
+    final prefs = await SharedPreferences.getInstance();
+    return prefs.getString('auth_token');
+  }
+
+
+  static Future<http.Response> login(String email, String password) async {
+    return await http.post(
+      Uri.parse('$_baseUrl/login'),
+      headers: {'Content-Type': 'application/json'},
+      body: json.encode({'email': email, 'password': password}),
+    );
+  }
+
+  static Future<http.Response> fetchData(String token) async {
+    return await http.get(
+      Uri.parse('$_baseUrl/protected-endpoint'),
+      headers: {
+        'Authorization': 'Bearer $token',
+        'Content-Type': 'application/json',
+      },
+    );
+  }
+}
