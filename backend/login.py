df14866c7a1d3dc0bb8d82d478c4d9acb5a755dc
AlnabulsiZ <zina.nabulsi04@gmail.com>
2025-04-02 09:12:18 +0300

    Update login.py
    JWT token added

--- backend/login.py
+++ backend/login.py
@@ -1,13 +1,34 @@
-from fastapi import APIRouter, HTTPException
+from fastapi import APIRouter, HTTPException, Depends
 from pydantic import BaseModel
 from werkzeug.security import check_password_hash
 from tables import get_db
+import jwt
+import os 
+import secrets
 
+from dotenv import load_dotenv
+from datetime import datetime, timedelta # => Validity time
+from fastapi.security import OAuth2PasswordBearer
+
+load_dotenv()
+secret_key=os.getenv("SECRET_KEY",secrets.token_hex(32))
+algo = "HS256"
+access_token_expire_time_minutes=60
+
+auth_token = OAuth2PasswordBearer(tokenUrl="login")
 rou = APIRouter()
 
 class LoginRequest(BaseModel):
     email: str
     password: str
+
+
+def create_access_token(data:dict,expire_delta:timedelta=timedelta(minutes=access_token_expire_time_minutes)):
+    to_encode=data.copy()
+    expire=datetime.utcnow() + expire_delta
+    to_encode.update({"exp":expire})
+    return jwt.encode(to_encode,secret_key,algorithm=algo)
+ 
 
 @rou.post("/login")
 def login(data: LoginRequest):
@@ -18,6 +39,22 @@ def login(data: LoginRequest):
     db.close()
     
     if user and check_password_hash(user[0], data.password):
-        return {"message": "Login successful", "status": "success"}
+        token=create_access_token({"sub":data.email})
+        return {"access token": token, "type": "bearer"}
     
     raise HTTPException(status_code=400, detail="Invalid email or password")
+
+
+@rou.post("/users/self")
+def get_current_user(token:str=Depends(auth_token)):
+    try:
+        payload= jwt.decode(token,secret_key,algorithms=[algo]) # [algo] => must be list, if not > TypeError
+        username : str = payload.get("sub")
+        if username is None:
+            raise HTTPException(status_code=401,detail="Invalid token")
+        return {"username":username}
+    
+    except jwt.ExpiredSignatureError: 
+        raise HTTPException(status_code=401,detail="Token expired")
+    except jwt.InvalidTokenError:
+        raise HTTPException(status_code=401, detail="Invalid token")
